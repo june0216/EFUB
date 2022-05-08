51a69b5fe49e691b8a4c074e5d105b08ebef1212
June <ziyun1612@naver.com>
2022-05-09 00:40:38 +0900

    Efub_spring_6주차

---
+++ src/main/java/ziyun/seminar/board/dto/BoardDto.java
@@ -0,0 +1,35 @@
+package ziyun.seminar.board.dto;
+
+
+import lombok.*;
+import ziyun.seminar.domain.entity.Board;
+
+@Setter
+@Getter
+@ToString
+@NoArgsConstructor
+public class BoardDto {
+    private Long id;
+    private String author;
+    private String title;
+    private String content;
+
+    @Builder
+    public BoardDto(Long id, String author, String title, String content) {
+        this.id = id;
+        this.author = author;
+        this.title = title;
+        this.content = content;
+
+    }
+
+    public Board toEntity() {
+        Board build = Board.builder()
+                .id(id)
+                .author(author)
+                .title(title)
+                .content(content)
+                .build();
+        return build;
+    }
+}
---
+++ src/main/java/ziyun/seminar/domain/repository/BoardRepository.java
@@ -0,0 +1,7 @@
+package ziyun.seminar.domain.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import ziyun.seminar.domain.entity.Board;
+
+public interface BoardRepository extends JpaRepository<Board, Long>{
+}
